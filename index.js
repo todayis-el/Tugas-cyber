d07402d688332a80140e5e7239cf6c0bc57de3c8
todayis-el <111452246+todayis-el@users.noreply.github.com>
2023-01-24 11:57:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ initializePassport(passport);
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'views')));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:false}));
