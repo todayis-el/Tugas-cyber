a024b56f58c01d4133ef15a1cfe3003945740517
todayis-el <111452246+todayis-el@users.noreply.github.com>
2023-01-24 11:59:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'views')));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended:false}));
-
 app.use(session({
     secret: 'secret',
 
