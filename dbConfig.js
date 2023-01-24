810180e847f7ef1eeac307d6b60a05c2d3db160e
todayis-el <111452246+todayis-el@users.noreply.github.com>
2023-01-24 11:36:47 +0800

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -4,10 +4,11 @@ const{Pool}= require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
+//const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = `mysql://5j9oqoy6r6fs:pscale_pw_EM1bLYjmVUpt9jvhQtPoP_Auvp77ZWR29s113O2Qbjg@oc9e50ejz9kq.ap-southeast-2.psdb.cloud/nodelogin?ssl={"rejectUnauthorized":true}`;
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+    //connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+    connectionString: `mysql://5j9oqoy6r6fs:pscale_pw_EM1bLYjmVUpt9jvhQtPoP_Auvp77ZWR29s113O2Qbjg@oc9e50ejz9kq.ap-southeast-2.psdb.cloud/nodelogin?ssl={"rejectUnauthorized":true}`
 });
 //tes
 module.exports = { pool };
